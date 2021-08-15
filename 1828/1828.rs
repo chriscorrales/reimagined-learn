67ee80ff9bcb638a8317d4a3841ba7ed2da93530
Christian Corrales <chriscorrales29@gmail.com>
2021-08-15 16:59:35 -0300

    feat: adicionados exercícios 1090, 1209, 1828 e 1837

---
+++ 1828/1828.rs
@@ -0,0 +1,15 @@
+/*
+    a tesoura corta o papel;
+    o papel embrulha a pedra;
+    a pedra esmaga o lagarto;
+    o lagarto envenena Spock;
+    Spock destrói a tesoura;
+    a tesoura decapita o lagarto;
+    o lagarto come o papel;
+    o papel contesta Spock;
+    Spock vaporiza a pedra;
+    a pedra quebra a tesoura.
+
+
+    https://www.urionlinejudge.com.br/judge/pt/problems/view/1828
+*/
