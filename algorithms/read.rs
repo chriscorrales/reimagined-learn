f74a7574c21b23f5f4618e92fd6e3d5d1ecd473e
Christian Corrales <chriscorrales29@gmail.com>
2021-08-15 16:13:28 -0300

    feat: adicionado funções de read

---
+++ algorithms/read.rs
@@ -0,0 +1,40 @@
+use std::io;
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ($out:ident as $type:ty) => {
+        let mut inner = String::new();
+        io::stdin().read_line(&mut inner).expect("A String");
+        let $out = inner.trim().parse::<$type>().expect("Parseble");
+    };
+}
+
+macro_rules! read_str {
+    ($out:ident) => {
+        let mut inner = String::new();
+        io::stdin().read_line(&mut inner).expect("A String");
+        let $out = inner.trim();
+    };
+}
+
+macro_rules! read_vec {
+    ($out:ident as $type:ty) => {
+        let mut inner = String::new();
+        io::stdin().read_line(&mut inner).unwrap();
+        let $out = inner
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse::<$type>().unwrap())
+            .collect::<Vec<$type>>();
+    };
+}
+
+fn main() {
+    read!(x as u32);
+    read!(y as f64);
+    read!(z as char);
+    println!("{} {} {}", x, y, z);
+
+    read_vec!(v as u32); // Reads space separated integers and stops when newline is encountered.
+    println!("{:?}", v);
+}
