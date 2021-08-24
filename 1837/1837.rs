248aad810530854d422979e1a00c7cec266376a2
Christian Corrales <chriscorrales29@gmail.com>
2021-08-23 23:41:35 -0300

    feat(1837): adicionado exercício 1837

--- 1837/1837.rs
+++ 1837/1837.rs
@@ -1,1 +1,20 @@
 // https://www.urionlinejudge.com.br/judge/pt/problems/view/1837
+use std::io;
+
+macro_rules! read_vec {
+    ($out:ident as $type:ty) => {
+        let mut inner = String::new();
+        io::stdin().read_line(&mut inner).unwrap();
+        let $out = inner
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse::<$type>().unwrap())
+            .collect::<Vec<$type>>();
+    };
+}
+
+fn main() {
+    read_vec!(v as f32);
+
+    println!("{} {}", (v[0] / v[1]).floor(), (v[0] % v[1]));
+}
